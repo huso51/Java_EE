6a8aed21baf4bfed835ab188f0c4dd7806ad37ad
huso51 <huseyinaydin99@gmail.com>
2022-11-16 11:58:45 +0300

    null

---
+++ 04_HubGit/src/main/java/com/mimaraslan/App.java
@@ -0,0 +1,21 @@
+package com.mimaraslan;
+
+public class App 
+{
+
+    public static void main( String[] args )
+    {
+    	
+    	Islem islem = new Islem();
+    	islem.goster();
+    	
+    	selamla();
+    }
+ 
+	private static void selamla() {
+		 System.out.println( "----------------" );		
+		 System.out.println( "Selam Github!" );		
+		 System.out.println( "----------------" );		
+	}
+	
+}
---
+++ 04_HubGit/src/main/java/com/mimaraslan/Islem.java
@@ -0,0 +1,12 @@
+package com.mimaraslan;
+
+public class Islem {
+	
+	int veriler [] = {1, 2, 3};
+	
+	public void goster() {
+		System.out.println("Sonuçları göster.");
+		System.out.println("------");
+	}
+
+}
